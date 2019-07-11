4a1a55abdc925faf4ef8770f9dbe19044d7d7d8f
Marcel Assis <marcel@marcelassis.com>
2019-07-11 00:33:42 -0300

    Adding repos

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = '53cd44d2dfc1c4f77bf2';
     this.client_secret = 'd4dc6788b160f18c9fec71deba55d4d5374e8be6';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -10,11 +12,20 @@ class Github {
         this.client_id
       }&client_secret=${this.client_secret}`
     );
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
 
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
--- ui.js
+++ ui.js
@@ -40,6 +40,35 @@ class UI {
     `;
   }
 
+  // show repos UI
+  showRepos(repos) {
+    let output = '';
+    console.log('resposssss', repos);
+    repos.forEach(function(repo) {
+      output += `
+      
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank" >${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+          <span class="badge badge-primary">Stars: ${
+            repo.stargazers_count
+          }</span>
+          <span class="badge badge-secondary">Watchers: ${repo.watchers}</span>
+          <span class="badge badge-secondary">Forks: ${repo.forks_count}</span>
+          </div>
+        </div>
+      </div>
+      
+      `;
+    });
+
+    // output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
   clearAlert() {
     const currentAlert = document.querySelector('.alert');
     if (currentAlert) {
